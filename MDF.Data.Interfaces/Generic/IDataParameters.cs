b05a8612f2ba464607cca5d74ff4bd7743e54e07
Vladislav Sudakov <sudakovmail@gmail.com>
2017-06-09 14:01:55 +0800

    made generic repository for our data layer, created poco classes for all of our tables, integrated ServiceStack.OrmLite

---
+++ MDF.Data.Interfaces/Generic/IDataParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace MDF.Data.Interfaces.Generic
+{
+    /// <summary>The DataParameters interface.</summary>
+    public interface IDataParameters
+    {
+        /// <summary>Gets the connection string.</summary>
+        string ConnectionString { get;  }
+    }
+}
---
+++ MDF.Data/Generic/OrmLiteGenericRepository.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MDF.Data.Interfaces.Generic;
+using ServiceStack.OrmLite;
+
+namespace MDF.Data.Generic
+{
+    /// <summary>The OrmLiteGenericRepository interface.</summary>
+    /// <typeparam name="T">Any reference type.</typeparam>
+    public interface IOrmLiteGenericRepository<T> : IGenericRepository<T>
+        where T : class
+    {
+        /// <summary>Gets the query so it could be run later.</summary>
+        /// <returns>The <see cref="SqlExpression"/>.</returns>
+        SqlExpression<T> GetQuery();
+    }
+
+    /// <summary>The orm lite generic repository.</summary>
+    /// <typeparam name="T">Any reference entity type.</typeparam>
+    public class OrmLiteGenericRepository<T> : IOrmLiteGenericRepository<T>
+        where T : class
+    {
+        /// <summary>The data parameters.</summary>
+        private readonly IDataParameters _parameters;
+
+        /// <summary>Initializes a new instance of the <see cref="OrmLiteGenericRepository{T}"/> class.</summary>
+        /// <param name="parameters">The parameters.</param>
+        public OrmLiteGenericRepository(IDataParameters parameters)
+        {
+            _parameters = parameters;
+        }
+
+        #region IOrmLiteGenericRepository
+         
+        public SqlExpression<T> GetQuery()
+        {
+            var factory = this.CreateFactory();
+            using (var db = factory.Open())
+            {
+                return db.From<T>();
+            }
+        }
+
+        #endregion
+
+        #region IGenericRepository
+         
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> expression)
+        {
+            var factory = this.CreateFactory();
+            using (var db = factory.Open())
+            {
+                var query = db.From<T>().Where(expression);
+                return await db.SingleAsync<T>(query);
+            }
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> expression = null)
+        {
+            var factory = this.CreateFactory();
+            using (var db = factory.Open())
+            {
+                var query = db.From<T>();
+
+                if (expression != null)
+                {
+                    query = query.Where(expression);
+                }
+
+                return await db.ColumnAsync<T>(query);
+            }
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            var factory = this.CreateFactory();
+            using (var db = factory.Open())
+            {
+                await db.InsertAsync(entity);
+            }
+        }
+
+        public async Task DeleteAsync(Expression<Func<T, bool>> expression)
+        {
+            var factory = this.CreateFactory();
+            using (var db = factory.Open())
+            {
+                await db.DeleteAsync(expression);
+            }
+        }
+
+        public async Task UpdateAsync(T entity, Expression<Func<T, bool>> expression)
+        {
+            var factory = this.CreateFactory();
+            using (var db = factory.Open())
+            {
+                await db.UpdateAsync(entity, expression);
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected OrmLiteConnectionFactory CreateFactory()
+        {
+            return new OrmLiteConnectionFactory(
+                _parameters.ConnectionString,
+                SqlServerDialect.Provider);
+        }
+
+        #endregion
+    }
+}
